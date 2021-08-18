dfac86f1e57d36c7c41d2db31a50535b8d7758f7
Lance Dang <lancedang64@gmail.com>
2021-08-18 16:18:21 +1000

    Fixed tests typo

--- src/factory-functions/Ship/index.test.js
+++ src/factory-functions/Ship/index.test.js
@@ -4,12 +4,12 @@ import Ship from './index';
 const stubShip = Ship('Submarrine', 3);
 
 describe('hit() method', () => {
-	test('it updates shipState', () => {
+	test('updates shipState', () => {
 		stubShip.hit(1);
 		expect(stubShip.shipState).toEqual(['safe', 'hit', 'safe']);
 	});
 
-	test('it does not change ship length', () => {
+	test('does not change ship length', () => {
 		stubShip.hit(99);
 		expect(stubShip.length).toBe(3);
 		expect(stubShip.shipState.length).toBe(3);
@@ -17,11 +17,11 @@ describe('hit() method', () => {
 });
 
 describe('getSunkStatus() method', () => {
-	test('it returns correct initial sunk status', () => {
+	test('returns correct initial sunk status', () => {
 		expect(stubShip.getSunkStatus()).toBeFalsy();
 	});
 
-	test('it returns correctly when ship is sunk', () => {
+	test('returns correctly when ship is sunk', () => {
 		stubShip.hit(0);
 		stubShip.hit(1);
 		stubShip.hit(2);
